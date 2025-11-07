7a9282e38a9d6ea44fb9e3ffcd7dee98c279a162
johntech-dev <johnayomide50@gmail.com>
2025-11-07 15:38:00 +0100

    updated

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -6,10 +6,10 @@ import { MessageCircle } from 'lucide-react'
 const HeroSection = () => {
     return (
         <section
-            className='w-full h-[90vh] min-h-[400px] max-h-[90vh] lg:max-h-[700px] xl:max-h-[600px] bg-[#181818] bg-cover bg-center flex justify-center items-center px-4 sm:px-0'
+            className='w-full h-[90vh] min-h-[500px] sm:min-h-[400px] max-h-[90vh] lg:max-h-[700px] xl:max-h-[600px] bg-[#181818] bg-cover bg-center flex justify-center items-center px-2 sm:px-4 md:px-0'
             style={{ backgroundImage: `url(${image.heroBg.src})` }}
         >
-            <div className='w-full px-4 sm:px-5 h-full flex flex-col justify-center items-center mx-auto gap-4 md:gap-2'>
+            <div className='w-full px-2 sm:px-4 md:px-5 h-full flex flex-col justify-center text-center items-center mx-auto gap-3 sm:gap-4 md:gap-2'>
                 {/* Mobile View - Three lines, uppercase */}
                 <h1 className='font-anton text-center font-normal text-white text-[64px] leading-[112%] tracking-normal uppercase md:text-[88px] md:capitalize md:w-[707px] md:h-[198px] w-[337px] h-[216px] mx-auto'>
                     <span className="md:hidden block">
@@ -19,9 +19,9 @@ const HeroSection = () => {
                         <br />
                         EVEN YOU
                     </span>
-                    {/* Desktop View - Two lines, capitalize */}
-                    <span className="hidden md:block">
-                        Jesus loves you,
+                    {/* Desktop View - Four lines, Title Case */}
+                    <span className="hidden md:flex text-center">
+                        Jesus Loves you,
                         <br />
                         yes you, even you
                     </span>
@@ -34,15 +34,13 @@ const HeroSection = () => {
                         FELLOWSHIP, AND PURPOSE.
                     </span>
                     {/* Desktop View - Single line */}
-                    <span className="hidden md:block">
+                    <span className="hidden md:block text-center">
                         WELCOME TO A PLACE OF INTIMACY, FELLOWSHIP, AND PURPOSE.
                     </span>
                 </p>
                 <Button 
-                    className='flex items-center gap-[10px] mt-8 sm:mt-10'
+                    className='flex items-center gap-[10px] mt-4 sm:mt-8 md:mt-10 w-[180px] sm:w-[208px] h-[48px] sm:h-[52px]'
                     style={{
-                        width: '208px',
-                        height: '52px',
                         borderRadius: '2000px',
                         border: '1px solid #FFFFFF',
                         background: 'rgba(255, 255, 255, 0.09)',
